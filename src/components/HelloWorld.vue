db673adb687e4169567113906c7230b2c6fdcae1
xuweifeng <yagel@qq.com>
2018-12-04 03:21:06 +0800

    树形菜单

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,8 @@
 <template>
 <div>
-  <!--<ul id="demo">-->
-    <!--<item-->
-      <!--class="item"-->
-      <!--:model="treeData">-->
-    <!--</item>-->
-  <!--</ul>-->
+  <div id="all">
+    <tree :model="root" :length="length"></tree>
+  </div>
   <router-link to="/img-load">图片上传代码</router-link>
   <br>
   <router-link to="/cropper">裁剪并上传头像1</router-link>
@@ -23,39 +20,63 @@
 
 </template>
 <script>
-  // import item from './tree'
+  import tree from './tree.vue'
   export default  {
     name:"tree1",
+    components:{
+      tree
+    },
     data() {
       return {
-        treeData : {
-          name: 'My Tree',
-          children: [
-            { name: 'hello' },
-            { name: 'wat' },
+        root:{
+          name:"根节点",
+          level:0,
+          isOpen:true,
+          children:[
             {
-              name: 'child folder',
-              children: [
+              name:"节点1",
+              level:1,
+              url:"/homePage/middle/navLeftFirst",
+              isOpen:false,
+              children:[
                 {
-                  name: 'child folder',
-                  children: [
-                    { name: 'hello' },
-                    { name: 'wat' }
-                  ]
+                  name:"节点1-1",
+                  level:2,
+                  isOpen:true,
+                  children:[]
                 },
-                { name: 'hello' },
-                { name: 'wat' },
                 {
-                  name: 'child folder',
-                  children: [
-                    { name: 'hello' },
-                    { name: 'wat' }
-                  ]
+                  name:"节点1-2",
+                  level:2,
+                  isOpen:true,
+                  children:[]
                 }
               ]
+            },
+            {
+              name:"节点2",
+              level:1,
+              url:"/homePage/middle/navLeftSecond",
+              isOpen:false,
+              children:[
+                {
+                  name:"节点2-1",
+                  level:2,
+                  isOpen:true,
+                  children:[]
+                },
+                {
+                  name:"节点2-2",
+                  level:2,
+                  isOpen:true,
+                  children:[]
+                }
+              ]
+
             }
           ]
-        }
+        },
+        length:2
     }
     },
     methods:{
